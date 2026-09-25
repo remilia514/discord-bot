0ec344466923faf11176265533af302bea36e136
蕾咪 <remiliascarlet514514@gmail.com>
2026-09-26 02:40:06 +0800

    active changes

--- src/events/clientReady/consoleLog.js
+++ src/events/clientReady/consoleLog.js
@@ -1,10 +1,18 @@
 const { ActivityType } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, '..', '..', 'quotes.json');
 
 module.exports = (client) => {
     console.log(`${client.user.tag} is online`);
 
-        client.user.setActivity({
-        name: '男娘淫夢共和國',
+    const data = fs.readFileSync(filePath, 'utf8');
+    const quotesArray = JSON.parse(data);
+    const count = quotesArray.length;
+
+    client.user.setActivity({
+        name: `已收錄 ${count} 篇複製文`,
         type: ActivityType.Playing
     });
-}
+};
