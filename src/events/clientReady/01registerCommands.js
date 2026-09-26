3fb714a52965791c6ead404bc17170e67b0b1e2c
蕾咪 <remiliascarlet514514@gmail.com>
2026-09-26 15:12:19 +0800

    a

--- src/events/clientReady/01registerCommands.js
+++ src/events/clientReady/01registerCommands.js
@@ -4,47 +4,43 @@ const getApplicationCommands = require('../../utils/getApplicationCommands');
 const getLocalCommands = require('../../utils/getLocalCommands');
 
 module.exports = async (client) => {
-    try {
-        const localCommands = getLocalCommands();
-        const applicationCommands = await getApplicationCommands(client, testServer);
+  try {
+    const localCommands = getLocalCommands();
+    const applicationCommands = getApplicationCommands(client, testServer);
 
-        for (const localCommand of localCommands) {
-            const { name, description, options } = localCommand;
+    for (const localCommand of localCommands) {
+      const { name, description, options } = localCommand;
 
-            const existingCommand = applicationCommands?.cache?.find(
-                (cmd) => cmd.name === name
-            );
+      const existingCommand = await applicationCommands?.cache?.find(
+        (cmd) => cmd.name === name
+      );
 
-            if (existingCommand) {
-                if (localCommand.deleted) {
-                    await existingCommand.delete(existingCommand.id);
-                    continue;
-                }
-                
-                if (arecommandsDifferent(existingCommand, localCommand)) {
-                    await existingCommand.edit(existingCommand.id, {
-                        description,
-                        options
-                    });
-
-                    console.log(`Edited command: ${name}`);
-                }
-            } else {
-                if (localCommand.deleted) {
-                    console.log(`Command ${name} is marked as deleted, skipping registration.`);
-                    continue;
-                }
-
-                await applicationCommands.create({
-                    name,
-                    description,
-                    options,
-                });
-
-                console.log(`Registered command: ${name}`);
-            }
+      if (existingCommand) {
+        if (localCommand.deleted) {
+          await existingCommand.delete(existingCommand.id);
+          continue;
         }
-    } catch (error) {
-        console.error(error);
+
+        if (arecommandsDifferent(existingCommand, localCommand)) {
+          await existingCommand.edit(existingCommand.id, { description, options });
+          console.log(`Edited command: ${name}`);
+        }
+      } else {
+        if (localCommand.deleted) {
+          console.log(`Command ${name} is marked as deleted, skipping registration.`);
+          continue;
+        }
+
+        await client.application.commands.create({
+          name,
+          description,
+          options,
+        });
+
+        console.log(`Registered command: ${name}`);
+      }
     }
+  } catch (error) {
+    console.error(error);
+  }
 };
